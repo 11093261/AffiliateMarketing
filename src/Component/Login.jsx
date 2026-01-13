6241af2a6ac3d67296d5fa075c4de29dd5137fdc
Godstimeconst{sellername,price,description <okechukwugodstime75@gmail.com>
2026-01-13 22:30:26 +0100

    promptcommit

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -110,7 +110,7 @@ const Login = () => {
         <div className="mt-6 text-center">
           <p className="text-gray-600">
             Don't have an account?{' '}
-            <Link to="/register" className="text-indigo-600 hover:text-indigo-500 font-medium">
+            <Link to="/signup" className="text-indigo-600 hover:text-indigo-500 font-medium">
               Sign up
             </Link>
           </p>
